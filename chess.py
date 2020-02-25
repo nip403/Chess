06ec200dadd46737cf7a2b708ddd6c01400bad89
nip403 <41174986+nip403@users.noreply.github.com>
2020-02-25 17:02:05 +0000

    Update chess.py

--- chess.py
+++ chess.py
@@ -1,12 +1,20 @@
-from engine import Chess
+from chess.engine import ChessText
+import os
 
 def main():
-    game = Chess()
+    #game = GraphicsEngine(Chess(text=False), [800, 800])
+    #game.play()
+
+    #""" text based game:
+    game = ChessText()
+
     while True:
         msg = game.input()
 
         if msg is not None:
             print(msg)
+
+    #"""
 
 if __name__ == "__main__":
     main()
